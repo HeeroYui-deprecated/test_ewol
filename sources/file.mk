2edbefd754b9a2071ed53599c226f8a324871136
Edouard DUPIN <yui.heero@gmail.com>
2012-12-23 19:33:56 +0100

    First application test

---
+++ sources/file.mk
@@ -0,0 +1,19 @@
+
+
+###############################################################################
+### Files Listes                                                            ###
+###############################################################################
+
+# Globals debug tool:
+FILE_LIST:=		appl/Debug.cpp \
+                appl/init.cpp \
+                appl/MainWindows.cpp \
+                appl/TestButton.cpp \
+                appl/TestButtonColor.cpp \
+                appl/TestLabel.cpp \
+                appl/TestScene.cpp
+
+LOCAL_COPY_FOLDERS := ../data/icon.*:theme/default \
+                      ../data/cube.*: \
+                      ../data/grass.*: \
+                      ../data/stone*: \
